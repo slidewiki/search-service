6ce2973b34893f6d8e45e5f7cf9ac4fc9e2aba05
Serafeim Chatzopoulos <s.chatzopoulos@gmail.com>
2016-10-23 23:40:28 +0300

    users collection

---
+++ application/mongoListeners/usersListener.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const MongoStream = require('mongo-trigger'),
+    users = require('../solr/objectCollections/users'),
+    mongoConfig = require('../configuration').mongoConfig;;
+
+module.exports = {
+    listen: function(){
+
+        // init data stream
+        let usersStream = new MongoStream({
+            format: 'pretty',
+            host: mongoConfig.HOST,
+            port: mongoConfig.PORT,
+        });
+
+        // watch slides collection
+        let usersCollection = mongoConfig.SLIDEWIKIDATABASE + '.users';
+        usersStream.watch(usersCollection, (event) => {
+            console.log('\nusers ' + JSON.stringify(event));
+
+            switch(event.operation){
+                case 'insert':
+                    users.new(event.data);
+                    break;
+                case 'update':
+                    users.update(event);
+                    break;
+            }
+        });
+    }
+};
---
+++ application/solr/objectCollections/users.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const solrClient = require('../solrClient'),
+    // microservices = require('../../microservices/microservicesConnection'),
+    co = require('../../common');
+
+module.exports = {
+
+    new: function(userDbObj){
+        // form root doc
+        let rootDoc = {};
+        rootDoc.solr_id = 'user_' + userDbObj._id;
+        rootDoc._id = userDbObj._id;
+        rootDoc.username = userDbObj.username;
+        rootDoc.surname = userDbObj.surname;
+        rootDoc.forename = userDbObj.forename;
+        rootDoc.email = userDbObj.email;
+        rootDoc.organization = userDbObj.organization;
+        rootDoc.kind = 'user';
+
+        console.log('new ' + JSON.stringify(rootDoc));
+        // solrClient.addDocs(rootDoc).then( (result) => solrClient.commit() );
+    },
+
+    update: function(userDbObj){
+        // update specified fields
+        if(!userDbObj.data.hasOwnProperty('$set')){
+            this.new(userDbObj.data);
+        }
+        else{
+            let updateObj = {};
+            for(let prop in userDbObj.data.$set){
+
+                if(prop === 'username' ||
+                    prop === 'surname' ||
+                    prop === 'forename' ||
+                    prop === 'email' ||
+                    prop === 'organization'){   //do not store active in root deck
+
+                    updateObj[prop] = {'set': userDbObj.data.$set[prop]};
+                }
+
+            }
+
+            // change made to root doc
+            if(!co.isEmpty(updateObj)){
+                updateObj.solr_id = 'user_' + userDbObj.targetId;
+                console.log('update ' + JSON.stringify(updateObj));
+
+                // solrClient.addDocs(updateObj).then( (result) => solrClient.commit() );
+            }
+        }
+
+    }
+
+};
