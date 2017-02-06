308bfe18ace281ab1b8e0a8f175419813a92038e
Kurt Junghanns <tboonx@googlemail.com>
2017-02-06 16:47:02 +0100

    [master] Fixed configuration.js

--- application/configuration.js
+++ application/configuration.js
@@ -1,6 +1,8 @@
 /* This module is used for confugrating the SOLR connection*/
 'use strict';
 
+const co = require('./common');
+
 //read solr URL from /etc/hosts
 let host = 'slidewiki.imis.athena-innovation.gr';
 const fs = require('fs');
@@ -22,7 +24,6 @@ if (host !== 'localhost')
     console.log('Using ' + host + ' as database host.');
 
 //read solr port from ENV
-const co = require('./common');
 let port = 8983;
 if (!co.isEmpty(process.env.SOLR_CONFIG_PORT)) {
     port = process.env.SOLR_CONFIG_PORT;
@@ -41,18 +42,7 @@ if (!co.isEmpty(process.env.SOLR_PATH)) {
     // console.log('Using path ' + path + ' as solr path.');
 }
 
-// deck service URI
-if (co.isEmpty(process.env.SERVICE_URL_DECK)) {
-    console.log('No SERVICE_URL_DECK env variable defined');
-    process.exit(1);
-}
 console.log('Connected with deck-service at: ' + process.env.SERVICE_URL_DECK);
-
-// user service URI
-if (co.isEmpty(process.env.SERVICE_URL_USER)) {
-    console.log('No SERVICE_URL_USER env variable defined');
-    process.exit(2);
-}
 console.log('Connected with user-service at: ' + process.env.SERVICE_URL_USER);
 
 let mongoHost = 'localhost';
