51e14fdb8a1a87851e27b59c5ef42f7e6c0e6ded
Serafeim Chatzopoulos <s.chatzopoulos@gmail.com>
2017-02-07 12:30:13 +0200

    deck flattening

--- application/solr/objectCollections/decks.js
+++ application/solr/objectCollections/decks.js
@@ -3,63 +3,101 @@
 const solrClient = require('../solrClient'),
     services = require('../../microservices/microservicesConnection');
 
-function newDeck(deckDbObj){
-    let newDocs = [];
-    // form root document
-    let rootDoc = {};
-    rootDoc.solr_id = 'deck_' + deckDbObj._id;
-    rootDoc._id = deckDbObj._id;
-    rootDoc.kind = 'deck';
-    rootDoc.timestamp = deckDbObj.timestamp;
-    rootDoc.description = deckDbObj.description;
-    // rootDoc.translation = deckDbObj.translation;
-    rootDoc.lastUpdate = deckDbObj.lastUpdate;
-    rootDoc.license = deckDbObj.license;
-    rootDoc.user = deckDbObj.user;
-    rootDoc.contributors = deckDbObj.contributors.map( (contr) => { return contr.user; });
 
-    // rootDoc.tags = deckDbObj.tags;
-    newDocs.push(rootDoc);
 
-    // form deck revision documents
-    for(let i=0; i<deckDbObj.revisions.length; i++){
-        let newRevision = newDeckRevision(deckDbObj._id, deckDbObj.active, deckDbObj.revisions[i]);
-        newDocs.push(newRevision);
-    }
+function newDeck(deckObj){
+    let newDocs = deckObj.revisions.map( (rev) => {
+        let doc = {
+            solr_id: 'deck_' + deckObj._id + '-' + rev.id,
+            solr_parent_id: 'deck_' + deckObj._id,
+            db_id: deckObj._id,
+            db_revision_id: rev.id,
+            timestamp: rev.timestamp,
+            lastUpdate: deckObj.lastUpdate,
+            kind: 'deck',
+            language: rev.language,
+            license: deckObj.license,
+            usage: rev.usage.map( (us) => { return us.id + '-' + us.revision; }),
+            creator: deckObj.user,
+            revision_owner: rev.user,
+            contributors: deckObj.contributors.map( (contr) => { return contr.user; }),
+            tags: rev.tags,
+            active: ((rev.id === deckObj.active) || (rev.usage.length > 0)) ? true : false,
+        };
+
+        // if language is undefined, set English (this should not happen in normal execution)
+        if(!rev.language){
+            rev.language = 'en_GB';
+        }
+
+        // language specific fields
+        doc['title_' + rev.language] = rev.title;
+        doc['description_' + rev.language] = deckObj.description;
+
+        return doc;
+    });
 
     return solrClient.addDocs(newDocs);
 }
 
-function newDeckRevision(parent_id, active, rev){
+// function newDeckOld(deckObj){
+//     let newDocs = [];
+//     // form root document
+//     let rootDoc = {};
+//     rootDoc.solr_id = 'deck_' + deckDbObj._id;
+//     rootDoc._id = deckDbObj._id;
+//     rootDoc.kind = 'deck';
+//     rootDoc.timestamp = deckDbObj.timestamp;
+//     rootDoc.description = deckDbObj.description;
+//     // rootDoc.translation = deckDbObj.translation;
+//     rootDoc.lastUpdate = deckDbObj.lastUpdate;
+//     rootDoc.license = deckDbObj.license;
+//     rootDoc.user = deckDbObj.user;
+//     rootDoc.contributors = deckDbObj.contributors.map( (contr) => { return contr.user; });
+//
+//     // rootDoc.tags = deckDbObj.tags;
+//     newDocs.push(rootDoc);
+//
+//     // form deck revision documents
+//     for(let i=0; i<deckDbObj.revisions.length; i++){
+//         let newRevision = newDeckRevision(deckDbObj._id, deckDbObj.active, deckDbObj.revisions[i]);
+//         newDocs.push(newRevision);
+//     }
+//
+//     return solrClient.addDocs(newDocs);
+//
+// }
 
-    let newDoc = {};
+// function newDeckRevisionOld(parent_id, active, rev){
+//
+//     let newDoc = {};
+//
+//     newDoc.solr_id = 'deck_revision_' + parent_id + '-' + rev.id;
+//     newDoc.solr_parent_id = 'deck_' + parent_id;
+//     newDoc.id = rev.id;
+//     newDoc.parent_id = parent_id;
+//     newDoc.kind = 'deck_revision';
+//     newDoc.timestamp = rev.timestamp;
+//     newDoc.user = rev.user;
+//     // newDoc.license = rev.license;
+//     newDoc.tags = rev.tags;
+//     newDoc.title = rev.title;
+//     newDoc.active = ((rev.id === active) || (rev.usage.length > 0)) ? true : false;
+//     newDoc.language = rev.language;
+//
+//     return newDoc;
+// }
 
-    newDoc.solr_id = 'deck_revision_' + parent_id + '-' + rev.id;
-    newDoc.solr_parent_id = 'deck_' + parent_id;
-    newDoc.id = rev.id;
-    newDoc.parent_id = parent_id;
-    newDoc.kind = 'deck_revision';
-    newDoc.timestamp = rev.timestamp;
-    newDoc.user = rev.user;
-    // newDoc.license = rev.license;
-    newDoc.tags = rev.tags;
-    newDoc.title = rev.title;
-    newDoc.active = ((rev.id === active) || (rev.usage.length > 0)) ? true : false;
-    newDoc.language = rev.language;
 
-    return newDoc;
-}
-
-
-function updateDeck(deckObj){
-
-
-    // update specified fields
-    if(!deckObj.data.hasOwnProperty('$set')){
-        return this.newDeck(deckObj.data);
-    }
-
-    return services.deckServiceRequest('deck', deckObj.targetId, newDeck);
+// function updateDeckOld(deckObj){
+//
+//
+//     // update specified fields
+//     if(!deckObj.data.hasOwnProperty('$set')){
+//         return this.newDeck(deckObj.data);
+//     }
+//
+//     return services.deckServiceRequest('deck', deckObj.targetId, newDeck);
 
     // let newDocs = [];
     // let updateObj = {};
@@ -89,10 +127,10 @@ function updateDeck(deckObj){
     // }
     //
     // return solrClient.addDocs(newDocs);
-}
+// }
 
 module.exports = {
     newDeck: newDeck,
-    newDeckRevision: newDeckRevision,
+    // newDeckRevision: newDeckRevision,
     updateDeck: updateDeck
 };
