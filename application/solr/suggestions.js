51e051c6ace03a0973324db01142e55c0cbbb69d
Serafeim Chatzopoulos <s.chatzopoulos@gmail.com>
2017-06-27 16:53:38 +0300

    highlight user suggestions

--- application/solr/suggestions.js
+++ application/solr/suggestions.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const solrClient = require('./solrClient');
+
 function escapeSpecialChars(s){
     return s.replace(/([\+\-!\(\)\{\}\[\]\^"~\*\?:\\])/g, (match) => {
         return '\\' + match;
@@ -10,15 +11,31 @@ function escapeSpecialChars(s){
     .replace(/'/g, '');
 }
 
+function getUserHighlight(user, userHighlight){
+    let username = (userHighlight.username) ? userHighlight.username : user.username;
+    let forename = (userHighlight.forename) ? userHighlight.forename : user.forename;
+    let surname = (userHighlight.surname) ? userHighlight.surname : user.surname;
+    return `${username}, ${forename} ${surname}`;
+}
+
+
 module.exports = {
 
     // finds users
     findUsers: function(q){
-
-        let queryString = 'q=' + escapeSpecialChars(encodeURIComponent(q)) + '*';
-
-        return Promise.resolve(solrClient.query(queryString, 'swSuggestUsers'));
-
+        let queryString = `q=${escapeSpecialChars(encodeURIComponent(q))}*&hl=true`;
+        
+        return solrClient.query(queryString, 'swSuggestUsers').then( (solrResponse) => {
+            let highlight = solrResponse.highlighting;
+ 
+            return solrResponse.response.docs.map( (user) => {
+                return {
+                    db_id: user.db_id, 
+                    username: user.username,
+                    highlight: getUserHighlight(user, highlight[user.solr_id])
+                };
+            });
+        });
     },
 
     // finds keywords
