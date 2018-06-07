8daebbb1f991cef96e4970aa7aa9e13952d97d6c
Serafeim Chatzopoulos <s.chatzopoulos@gmail.com>
2018-06-07 12:11:53 +0300

    Add index for agenda unique check

--- application/database/createIndexes.js
+++ application/database/createIndexes.js
@@ -1,11 +1,16 @@
 'use strict';
 
 const helper = require('./helper');
+const { agendaConfig } = require('../configuration');
 
 // this function should include commands that create indexes (if any)
 // for any collections that the service may be using
 
 // it should always return a promise
 module.exports = function() {
-    return Promise.resolve();
+    return helper.getCollection(agendaConfig.AGENDA_JOBS_COLLECTION).then((agenda) => {
+        return agenda.createIndexes([
+            { key: { 'data.id': 1, 'data.type': 1, 'data.event': 1 } },
+        ]);
+    });
 };
